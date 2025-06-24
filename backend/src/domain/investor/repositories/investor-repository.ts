fc5a21b2f6d37aa0ed6b46a732813b77c9d655f1
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-24 18:48:51 -0300

    feat: create investor repository

---
+++ backend/src/domain/investor/repositories/investor-repository.ts
@@ -0,0 +1,10 @@
+import { Investor } from "../entities/investor";
+
+export interface InvestorRepository {
+    findById(id: string): Promise<Investor | null>
+    findByEmail(email: string): Promise<Investor | null>
+    findByCpf(cpf: string): Promise<Investor | null>
+    create(investor: Investor): Promise<void>
+    update(investor: Investor): Promise<void>
+    delete(investor: Investor): Promise<void>
+}
