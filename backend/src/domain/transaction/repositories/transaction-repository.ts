90d5c8a85a1713b78d3f827112b51aa70335056d
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 10:41:08 -0300

    feat: add findByPortfolioAndAsset method

--- backend/src/domain/transaction/repositories/transaction-repository.ts
+++ backend/src/domain/transaction/repositories/transaction-repository.ts
@@ -5,6 +5,7 @@ export interface TransactionRepository {
     findById(id: string): Promise<Transaction | null>
     findManyByUserId(userId: string, params: PaginationParams): Promise<Transaction[]>
     findManyByAssetName(assetName: string, params: PaginationParams): Promise<Transaction[]>
+    findByPortfolioAndAsset(portfolioId: string, assetId: string): Promise<Transaction[]>
     create(transaction: Transaction): Promise<void>
     update(transaction: Transaction): Promise<void>
     delete(transaction: Transaction): Promise<void>
