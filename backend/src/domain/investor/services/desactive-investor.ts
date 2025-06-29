18d44f1d7861a8610de181ec9ac9cc3c580f107e
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 10:01:56 -0300

    feat: create desactive investor service

---
+++ backend/src/domain/investor/services/desactive-investor.ts
@@ -0,0 +1,31 @@
+import { Either, left, right } from "@/core/either"
+import { InvestorRepository } from "../repositories/investor-repository"
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error"
+
+interface DesactiveInvestorServiceRequest {
+    investorId: string
+}
+
+type DesactiveInvestorServiceResponse = Either<ResourceNotFoundError, {
+    message: string
+}>
+
+export class DesactiveInvestorService {
+    constructor(private investorRepository: InvestorRepository) {}
+
+    async execute({
+        investorId
+    }: DesactiveInvestorServiceRequest): Promise<DesactiveInvestorServiceResponse> {
+        const investor = await this.investorRepository.findById(investorId)
+        
+        if (!investor) return left(new ResourceNotFoundError())
+
+        investor.desactive()
+
+        await this.investorRepository.update(investor)
+
+        return right({
+            message: 'The investor has been desactive.'
+        })
+    }
+}
