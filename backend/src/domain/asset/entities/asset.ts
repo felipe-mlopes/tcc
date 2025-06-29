23890a0721197d3ce2bdbdfc24dd903e7bd19005
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 10:00:07 -0300

    feat: update asset entity

--- backend/src/domain/asset/entities/asset.ts
+++ backend/src/domain/asset/entities/asset.ts
@@ -1,7 +1,8 @@
 import { AggregateRoot } from "@/core/entities/aggregate-root";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
 
-enum AssetType {
+export enum AssetType {
     Stock = "Stock",
     ETF = "ETF",
     FIIs = "FIIs",
@@ -17,7 +18,67 @@ export interface AssetProps {
     sector: string,
     exchange: string,
     currency: string,
+    createdAt: Date,
+    updatedAt?: Date | null,
     isActive: boolean
 }
 
-export class Asset extends AggregateRoot<AssetProps> {}
+export class Asset extends AggregateRoot<AssetProps> {
+    public get assedId() {
+        return this.props.asserId
+    }
+
+    public get symbol() {
+        return this.props.symbol
+    }
+    
+    public get name() {
+        return this.props.name
+    }
+    
+    public get sector() {
+        return this.props.sector
+    }
+    
+    public get exchange() {
+        return this.props.exchange
+    }
+    
+    public get currency() {
+        return this.props.currency
+    }
+
+    public get createdAt() {
+        return this.props.createdAt
+    }
+
+    public get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    public get isActive() {
+        return this.props.isActive
+    }
+    
+    private touch() {
+        this.props.updatedAt = new Date()
+    }
+
+    public desactive() {
+        this.props.isActive = false
+        this.touch()
+    }
+
+    public static create(props: Optional<AssetProps, 'createdAt' | 'isActive'>, id?: UniqueEntityID) {
+        const asset = new Asset(
+            {
+                ...props,
+                createdAt: props.createdAt ?? new Date(),
+                isActive: true
+            }, 
+            id
+        )
+    
+        return asset
+    }
+}
