75ba9d806b29132b8e87e9aa8ca6c8122655ba3a
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 19:08:36 -0300

    refactor: remove delete method and change findByUserId to findByInvestorId method

--- backend/src/domain/portfolio/repositories/portfolio-repository.ts
+++ backend/src/domain/portfolio/repositories/portfolio-repository.ts
@@ -2,8 +2,7 @@ import { Portfolio } from "../entities/portfolio"
 
 export interface PortfolioRepository {
     findById(id: string): Promise<Portfolio | null>
-    findByUserId(userId: string): Promise<Portfolio | null>
+    findByInvestorId(userId: string): Promise<Portfolio | null>
     create(portfolio: Portfolio): Promise<void>
     update(portfolio: Portfolio): Promise<void>
-    delete(portfolio: Portfolio): Promise<void>
 }
