f11fa548103f8cc5adbf976ed928380df6dedf2d
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 09:59:37 -0300

    feat: delete unused entities and repositories files

--- backend/src/domain/analytics/repositories/performance-report-repository.ts
+++
@@ -1,8 +0,0 @@
-import { PerformanceReport } from "../entities/performance-report"
-
-export interface PerformanceReportRepository {
-    findById(id: string): Promise<PerformanceReport | null>
-    create(performanceReport: PerformanceReport): Promise<void>
-    update(performanceReport: PerformanceReport): Promise<void>
-    delete(performanceReport: PerformanceReport): Promise<void>
-}
