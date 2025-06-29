57a7000d82344e194694e76be7c45bc6e04a29d0
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 10:09:35 -0300

    fix: changes to correct updateName and updateEmail methods

--- backend/src/domain/investor/services/update-investor.ts
+++ backend/src/domain/investor/services/update-investor.ts
@@ -2,8 +2,6 @@ import { Either, left, right } from "@/core/either"
 import { Investor } from "../entities/investor"
 import { InvestorRepository } from "../repositories/investor-repository"
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error"
-import { Name } from "@/domain/value-objects/name"
-import { Email } from "@/domain/value-objects/email"
 import { NotAllowedError } from "@/core/errors/not-allowed-error"
 
 interface UpdateInvestorServiceRequest {
@@ -25,19 +23,16 @@ export class UpdateInvestorService {
         email
     }: UpdateInvestorServiceRequest): Promise<UpdateInvestorServiceResponse> {
         const investor = await this.investorRepository.findById(investorId)
-
         if (!investor) return left(new ResourceNotFoundError())
 
         if(name == undefined && email == undefined) return left(new NotAllowedError())
 
         if(!!name) {
-            const newName = Name.create(name)
-            investor.updateName(newName)
+            investor.updateName(name)
         }
 
         if(!!email) {
-            const newEmail = Email.create(email)
-            investor.updateEmail(newEmail)
+            investor.updateEmail(email)
         }
 
         await this.investorRepository.update(investor)
