76cbeea2e9ce0cf245ee572e4df1cc4c36aaa721
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 10:06:35 -0300

    refactor: uptdate to change findManyByPortfolioId to findManyByAssetName

--- backend/src/domain/transaction/repositories/transaction-repository.ts
+++ backend/src/domain/transaction/repositories/transaction-repository.ts
@@ -3,8 +3,8 @@ import { Transaction } from "../entities/transaction"
 
 export interface TransactionRepository {
     findById(id: string): Promise<Transaction | null>
-    findManyByPortfolioId(portfolioId: string, params: PaginationParams): Promise<Transaction[]>
     findManyByUserId(userId: string, params: PaginationParams): Promise<Transaction[]>
+    findManyByAssetName(assetName: string, params: PaginationParams): Promise<Transaction[]>
     create(transaction: Transaction): Promise<void>
     update(transaction: Transaction): Promise<void>
     delete(transaction: Transaction): Promise<void>
