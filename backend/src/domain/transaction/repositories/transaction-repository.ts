ec9671ff12126b8ba702f7562239b0bc2a00b7e6
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 19:10:28 -0300

    refactor: change findManyByUserId to findManyByPortfolioId method, findByPortfolioAndAsset to findByManyPortfolioAndAsset method and transaction to transactionId in delete method, and remove findManyByAssetName method

--- backend/src/domain/transaction/repositories/transaction-repository.ts
+++ backend/src/domain/transaction/repositories/transaction-repository.ts
@@ -3,10 +3,13 @@ import { Transaction } from "../entities/transaction"
 
 export interface TransactionRepository {
     findById(id: string): Promise<Transaction | null>
-    findManyByUserId(userId: string, params: PaginationParams): Promise<Transaction[]>
-    findManyByAssetName(assetName: string, params: PaginationParams): Promise<Transaction[]>
-    findByPortfolioAndAsset(portfolioId: string, assetId: string): Promise<Transaction[]>
+    findManyByPortfolioId(portfolioId: string, params: PaginationParams): Promise<Transaction[]>
+    findByManyPortfolioAndAsset(
+        portfolioId: string, 
+        assetId: string, 
+        params: PaginationParams
+    ): Promise<Transaction[]>
     create(transaction: Transaction): Promise<void>
     update(transaction: Transaction): Promise<void>
-    delete(transaction: Transaction): Promise<void>
+    delete(transactionId: string): Promise<void>
 }
