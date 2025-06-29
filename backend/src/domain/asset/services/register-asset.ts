294a2fd4fd0de3eca4948742a0b815b30474dd28
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-06-29 10:00:28 -0300

    feat: create register asset service

---
+++ backend/src/domain/asset/services/register-asset.ts
@@ -0,0 +1,47 @@
+import { Either, right } from "@/core/either";
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error";
+import { Asset, AssetType } from "../entities/asset";
+import { AssetRepository } from "../repositories/asset-repository";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+interface RegisterAssetServiceRequest {
+    symbol: string,
+    name: string,
+    assetType: AssetType,
+    sector: string,
+    exchange: string,
+    currency: string
+}
+
+type RegisterAssetServiceResponse = Either<ResourceNotFoundError, {
+    newAsset: Asset
+}>
+
+export class RegisterAssetService {
+    constructor(private assetRepository: AssetRepository) {}
+
+    async execute({
+        symbol,
+        name,
+        assetType,
+        sector,
+        exchange,
+        currency
+    }: RegisterAssetServiceRequest): Promise<RegisterAssetServiceResponse> {
+        const newAsset = Asset.create({
+            asserId: new UniqueEntityID(),
+            symbol,
+            name,
+            assetType,
+            sector,
+            exchange,
+            currency
+        })
+
+        await this.assetRepository.create(newAsset)
+
+        return right({
+            newAsset
+        })
+    }
+}
