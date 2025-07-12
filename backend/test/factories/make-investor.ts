50731173f8316760438f8abc192c6ca8e24db4b4
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-07-12 16:13:04 -0300

    test: include locale in Faker and fix to CPF valid text

--- backend/test/factories/make-investor.ts
+++ backend/test/factories/make-investor.ts
@@ -4,7 +4,11 @@ import { DateOfBirth } from "@/core/value-objects/date-of-birth";
 import { Email } from "@/core/value-objects/email";
 import { Name } from "@/core/value-objects/name";
 import { Investor, InvestorProfile, InvestorProps } from "@/domain/investor/entities/investor";
-import { faker } from '@faker-js/faker'
+import { Faker, faker, pt_BR } from '@faker-js/faker'
+
+export const customFaker = new Faker({
+    locale: [pt_BR]
+})
 
 export function makeInvestor(
     override: Partial<InvestorProps> = {},
@@ -12,7 +16,7 @@ export function makeInvestor(
 ) {
     const fakerName = Name.create(faker.person.fullName())
     const fakerEmail = Email.create(faker.internet.email())
-    const fakerCpf = CPF.create('999.999.999-99')
+    const fakerCpf = CPF.create("111.444.689-35")
     const fakerDate = DateOfBirth.create(faker.date.birthdate())
 
     const investor = Investor.create(
