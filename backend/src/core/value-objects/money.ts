554583891c28043898f4377c4a4a368b586f082b
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-07-20 10:45:35 -0300

    update: change Error to NotAllowedError in validations and fix multiply method

--- backend/src/core/value-objects/money.ts
+++ backend/src/core/value-objects/money.ts
@@ -1,9 +1,11 @@
+import { NotAllowedError } from "../errors/not-allowed-error"
+
 export class Money {
     private constructor(    
         private readonly amount: number,
         private readonly currency: string
     ) {
-        if (!currency || currency.length !== 3) throw new Error('Currency must be a 3-letter code.')
+        if (!currency || currency.length !== 3) throw new NotAllowedError('Currency must be a 3-letter code.')
     }
 
     static create(amount: number, currency: string = "BRL"): Money {
@@ -35,13 +37,13 @@ export class Money {
     }
 
     public multiply(factor: number): Money {
-        if (factor < 0) throw new Error('Multiplication factor cannot be negative.')
+        if (factor == 0) throw new NotAllowedError('Multiplication factor cannot be zero.')
 
         return new Money(this.amount * factor, this.currency)
     }
 
     public divide(divisor: number): Money {
-        if (divisor <= 0) throw new Error('Division by zero or negative number.')
+        if (divisor <= 0) throw new NotAllowedError('Division by zero or negative number.')
 
         return new Money(this.amount / divisor, this.currency)
     }
@@ -63,6 +65,6 @@ export class Money {
     }
 
     private validadeSameCurrency(valeu: Money): void {
-        if (this.currency !== valeu.currency) throw new Error(`Cannot operate with different currencies: ${this.currency} and ${valeu.currency}.`)
+        if (this.currency !== valeu.currency) throw new NotAllowedError(`Cannot operate with different currencies: ${this.currency} and ${valeu.currency}.`)
     }
 }
