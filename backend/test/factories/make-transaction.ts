d1da33b90f3ebd6b00bdb58b228ec09315fc5487
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-07-26 15:37:19 -0300

    test: refactor to change quantity, income, fees and totalAmount fakers

--- backend/test/factories/make-transaction.ts
+++ backend/test/factories/make-transaction.ts
@@ -9,19 +9,42 @@ export function makeTransaction(
     transactionType: TransactionType,
     id?: UniqueEntityID
 ) {
-    const fakerQuantity = Quantity.create(faker.number.int())
-    const fakerPrice = Money.create(faker.number.float())
-    const fakerFees = Money.create(faker.number.float())
+
+    let quantityFaker: Quantity
+    let incomeFaker: Money
+    let feesFaker: Money
+    let totalAmountFaker: Money
+
+    const priceFaker = Money.create(faker.number.float({
+        fractionDigits: 2
+    }))
+
+    if (transactionType !== TransactionType.Dividend) {
+        quantityFaker = Quantity.create(faker.number.int())
+        incomeFaker = Money.zero()
+        feesFaker = Money.create(faker.number.float({
+            fractionDigits: 2
+        }))
+        totalAmountFaker = priceFaker.multiply(quantityFaker.getValue())
+    } else {
+        quantityFaker = Quantity.zero()
+        incomeFaker = Money.create(faker.number.float({
+            fractionDigits: 2
+        }))
+        feesFaker = Money.zero()
+        totalAmountFaker = Money.zero()
+    }
 
     const transaction = Transaction.create(
         {
             portfolioId: new UniqueEntityID(),
             assetId: new UniqueEntityID(),
             transactionType: transactionType,
-            quantity: fakerQuantity,
-            price: fakerPrice,
-            totalAmount: fakerPrice.multiply(fakerPrice.getAmount()),
-            fees: fakerFees,
+            quantity: quantityFaker,
+            price: priceFaker,
+            income: incomeFaker,
+            totalAmount: totalAmountFaker,
+            fees: feesFaker,
             dateAt: new Date(),
             ...override
         },
