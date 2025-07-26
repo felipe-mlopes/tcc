4b4ef5e8fbe6596c3c7e9f589899fd8b04a672d7
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-07-26 17:35:23 -0300

    test: remove unused test to validate transaction if quantity is zero and test to validate transaction if fees is zero

--- backend/src/domain/transaction/services/transaction-validator.spec.ts
+++ backend/src/domain/transaction/services/transaction-validator.spec.ts
@@ -128,35 +128,6 @@ describe('Transaction Validator Service', () => {
             expect(result.value.message).toBe('Portfolio not found.')
         }
     })
-
-    it('should be not able to validate transaction if quantity is zero', async () => {
-        
-        // Arrange
-        const investor = makeInvestor()
-        const asset = makeAsset({ name: 'AAPL34' })
-        const portfolio = makePortfolio({ investorId: investor.id })
-
-        await inMemoryInvestorRepository.create(investor)
-        await inMemoryAssetRepository.create(asset)
-        await inMemoryPortfolioRepository.create(portfolio)
-
-        // Act
-        const result = await sut.validate({
-            investorId: investor.id.toValue().toString(),
-            assetName: asset.name,
-            quantity: 0,
-            price: 100,
-            fees: 1
-        })
-
-        // Assert
-        expect(result.isLeft()).toBe(true)
-        if (result.isLeft()) {
-            expect(result.value).toBeInstanceOf(NotAllowedError)
-            expect(result.value.message).toBe('Quantity must be greater than zero.')
-        }
-    })
-
     it('should be not able to validate transaction if price is zero', async () => {
         
         // Arrange
@@ -182,34 +153,6 @@ describe('Transaction Validator Service', () => {
         if (result.isLeft()) {
             expect(result.value).toBeInstanceOf(NotAllowedError)
             expect(result.value.message).toBe('Price must be greater than zero.')
-        }
-    })
-
-    it('should be not able to validate transaction if fees is zero', async () => {
-        
-        // Arrange
-        const investor = makeInvestor()
-        const asset = makeAsset({ name: 'AAPL34' })
-        const portfolio = makePortfolio({ investorId: investor.id })
-
-        await inMemoryInvestorRepository.create(investor)
-        await inMemoryAssetRepository.create(asset)
-        await inMemoryPortfolioRepository.create(portfolio)
-
-        // Act
-        const result = await sut.validate({
-            investorId: investor.id.toValue().toString(),
-            assetName: asset.name,
-            quantity: 10,
-            price: 100,
-            fees: 0
-        })
-
-        // Assert
-        expect(result.isLeft()).toBe(true)
-        if (result.isLeft()) {
-            expect(result.value).toBeInstanceOf(NotAllowedError)
-            expect(result.value.message).toBe('Fees must be greater than zero.')
         }
     })
 })
