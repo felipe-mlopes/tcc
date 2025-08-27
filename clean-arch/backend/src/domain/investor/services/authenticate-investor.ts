039a46a12fae01eb321bcbf28712c8238d3e9769
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-08-27 11:00:12 -0300

    feat: create authenticate investor service

---
+++ clean-arch/backend/src/domain/investor/services/authenticate-investor.ts
@@ -0,0 +1,56 @@
+import { Injectable } from "@nestjs/common"
+
+import { Either, left, right } from "@/core/either"
+import { WrongCredentialsError } from "@/core/errors/wrong-credentials-error"
+import { InvestorRepository } from "../repositories/investor-repository"
+import { HashComparer } from "../cryptography/hash-comparer"
+import { Encrypter } from "../cryptography/encrypter"
+
+interface AuthenticateInvestorServiceRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateInvestorServiceResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateInvestorService {
+  constructor(
+    private investorRepository: InvestorRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateInvestorServiceRequest): Promise<AuthenticateInvestorServiceResponse> {
+    const investor = await this.investorRepository.findByEmail(email)
+
+    if (!investor) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      investor.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: investor.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
